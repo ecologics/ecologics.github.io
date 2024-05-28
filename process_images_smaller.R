dba65bb84af63363c7c6cbb6bb40ba962785de7d
Paul Galpern <pgalpern@ucalgary.ca>
2024-05-28 11:52:42 -0600

    More tweaks to image size

--- process_images_smaller.R
+++ process_images_smaller.R
@@ -13,8 +13,15 @@ resize_images <- function(sourcePath, destPath, width, quality=90) {
     image <- image_read(file)
     image <- image_resize(image, paste0(width, "x"))
     output_file <- sub("\\.[^.]*$", ".jpg", file)
-    output_file <- sub(sourcePath, destPath, output_file)
     image_write(image, output_file, format="jpg", quality=quality)
   }
 }
 
+
+## Manually compress and turn images to JPG
+downsample_one_image_jpg <- function(sourceFile, quality=90) {
+  image <- image_read(sourceFile)
+  output_file <- sub("\\.[^.]*$", ".jpg", sourceFile)
+  output_file <- sub("(.*)(\\.[^.]+$)", "\\1s\\2", output_file)
+  image_write(image, output_file, format="jpg", quality=quality)
+}
